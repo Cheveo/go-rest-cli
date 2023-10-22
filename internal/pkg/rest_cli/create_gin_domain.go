8c190a145bb0ef75ee207bd519b09b20e4b3b76d
Sagato <danyal.iqbal@cheveo.de>
2023-10-22 23:14:54 +0200

    feat(cli): refactor gin domain to use struct methods.

--- internal/pkg/rest_cli/create_gin_domain.go
+++ internal/pkg/rest_cli/create_gin_domain.go
@@ -8,59 +8,67 @@ import (
 )
 
 type ginDomain struct {
-	Domain *types.DomainTmpl
+	DomainTmpl *types.DomainTmpl
 }
 
-func (s *ginDomain) Create() error {
-	err := util.CreateFileSkeleton(createHandler(s.Domain))
+func (gd *ginDomain) Create() error {
+	err := util.CreateFileSkeleton(gd.createHandler())
 
-	err = util.CreateFileSkeleton(createService(s.Domain))
-	err = util.CreateFileSkeleton(createStorage(s.Domain))
-	err = util.CreateFileSkeleton(util.CreateModel(s.Domain))
+	err = util.CreateFileSkeleton(gd.createService())
+	err = util.CreateFileSkeleton(gd.createStorage())
+	err = util.CreateFileSkeleton(gd.createModel())
 
 	return err
 }
 
 func NewGinDomain(d *types.DomainTmpl) *ginDomain {
 	return &ginDomain{
-		Domain: d,
+		DomainTmpl: d,
 	}
 }
 
-func createHandler(domain *types.DomainTmpl) []*types.FileInputs {
-	filePath := fmt.Sprintf("%s/%s", domain.Domain, "handler")
-	handlerFileName := fmt.Sprintf("%s_%s.go", domain.Domain, "handler")
+func (gd *ginDomain) createHandler() []*types.FileInputs {
+	filePath := fmt.Sprintf("%s/%s", gd.DomainTmpl.Domain, "handler")
+	handlerFileName := fmt.Sprintf("%s_%s.go", gd.DomainTmpl.Domain, "handler")
 
-	handlerFileInput := util.CreateFileInput(domain, "templates/gin/handler_tmpl.txt", filePath, handlerFileName)
-	handlerInterfaceFileInput := util.CreateFileInput(domain, "templates/handler_interface_tmpl.txt", filePath, "handler.go")
+	handlerFileInput := util.CreateFileInput(gd.DomainTmpl, "templates/gin/handler_tmpl.txt", filePath, handlerFileName)
+	handlerInterfaceFileInput := util.CreateFileInput(gd.DomainTmpl, "templates/handler_interface_tmpl.txt", filePath, "handler.go")
 
 	return []*types.FileInputs{handlerFileInput, handlerInterfaceFileInput}
 }
-func createService(domain *types.DomainTmpl) []*types.FileInputs {
-	filePath := fmt.Sprintf("%s/%s", domain.Domain, "service")
-	serviceFileName := fmt.Sprintf("%s_%s.go", domain.Domain, "service")
+func (gd *ginDomain) createService() []*types.FileInputs {
+	filePath := fmt.Sprintf("%s/%s", gd.DomainTmpl.Domain, "service")
+	serviceFileName := fmt.Sprintf("%s_%s.go", gd.DomainTmpl.Domain, "service")
 
-	serviceFileInput := util.CreateFileInput(domain, "templates/service_tmpl.txt", filePath, serviceFileName)
-	serviceInterfaceFileInput := util.CreateFileInput(domain, "templates/service_interface_tmpl.txt", filePath, "service.go")
+	serviceFileInput := util.CreateFileInput(gd.DomainTmpl, "templates/service_tmpl.txt", filePath, serviceFileName)
+	serviceInterfaceFileInput := util.CreateFileInput(gd.DomainTmpl, "templates/service_interface_tmpl.txt", filePath, "service.go")
 
 	return []*types.FileInputs{serviceFileInput, serviceInterfaceFileInput}
 }
-func createStorage(domain *types.DomainTmpl) []*types.FileInputs {
-	filePath := fmt.Sprintf("%s/%s", domain.Domain, "storage")
-	storageFileName := fmt.Sprintf("%s_%s.go", domain.Domain, "storage")
+func (gd *ginDomain) createStorage() []*types.FileInputs {
+	filePath := fmt.Sprintf("%s/%s", gd.DomainTmpl.Domain, "storage")
+	storageFileName := fmt.Sprintf("%s_%s.go", gd.DomainTmpl.Domain, "storage")
 
 	storageFileInput := util.CreateFileInput(
-		domain,
-		fmt.Sprintf("%s/storage_tmpl.txt", domain.TemplatePath),
+		gd.DomainTmpl,
+		fmt.Sprintf("%s/storage_tmpl.txt", gd.DomainTmpl.TemplatePath),
 		filePath,
 		storageFileName,
 	)
 	storageInterfaceFileInput := util.CreateFileInput(
-		domain,
-		fmt.Sprintf("%s/storage_interface_tmpl.txt", domain.TemplatePath),
+		gd.DomainTmpl,
+		fmt.Sprintf("%s/storage_interface_tmpl.txt", gd.DomainTmpl.TemplatePath),
 		filePath,
 		"storage.go",
 	)
 
 	return []*types.FileInputs{storageFileInput, storageInterfaceFileInput}
+}
+func (gd *ginDomain) createModel() []*types.FileInputs {
+	filePath := fmt.Sprintf("%s/%s", gd.DomainTmpl.Domain, "models")
+	modelFileName := fmt.Sprintf("%s_%s.go", gd.DomainTmpl.Domain, "models")
+
+	modelFileInput := util.CreateFileInput(gd.DomainTmpl, "templates/gin/model_tmpl.txt", filePath, modelFileName)
+
+	return []*types.FileInputs{modelFileInput}
 }
